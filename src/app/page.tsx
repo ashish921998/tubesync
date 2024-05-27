8d62cbba50494ad3d6fb08f4d403812a920d0266
Ashish Huddar <ashish@zomentum.com>
2024-05-27 11:05:52 +0530

    deploy change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Page() {
 							</div>
 						</CardContent>
 						<CardFooter className="text-center text-sm">
-							Don't have an account?
+							Dont have an account?
 							<Link className="underline" href="#">
 								Sign up
 							</Link>
